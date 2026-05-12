09de245cf5d76028232b7893c9887ff784e0b6b5
Sven van Heugten <sven@memcmp.org>
2026-05-12 06:37:08 +0200

    Refuse to run if git repo is dirty

--- Mutannot/Program.fs
+++ Mutannot/Program.fs
@@ -1,2 +1,17 @@
+open Fli
+
 [<EntryPoint>]
-let main argv = 0
+let main argv =
+    let gitState =
+        cli {
+            Exec "git"
+            Arguments [ "status"; "--porcelain" ]
+        }
+        |> Command.execute
+        |> Output.throwIfErrored
+
+    if gitState.Text <> None then
+        eprintfn "Uncommitted changes. Refusing to run."
+        exit 1
+
+    0
