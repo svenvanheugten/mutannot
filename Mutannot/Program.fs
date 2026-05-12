542adfcfc980ab323dd0ddc93161ab781cbcaa71
Sven van Heugten <sven@memcmp.org>
2026-05-12 07:05:45 +0200

    Build the project

--- Mutannot/Program.fs
+++ Mutannot/Program.fs
@@ -1,3 +1,5 @@
+open System
+open System.IO
 open Fli
 
 [<EntryPoint>]
@@ -17,5 +19,14 @@ let main argv =
     if gitState.Text <> None then
         eprintfn "Uncommitted changes. Refusing to run."
         exit 2
+
+    cli {
+        Exec "dotnet"
+        Arguments [ "build"; argv[0] ]
+        Output(new StreamWriter(Console.OpenStandardOutput()))
+    }
+    |> Command.execute
+    |> Output.throwIfErrored
+    |> ignore
 
     0
