fddbd612a8592d99f1892cebbeb90e010f9416e6
Sven van Heugten <sven@memcmp.org>
2026-07-08 19:25:38 +0200

    Make `nix build` run the tests

--- flake.nix
+++ flake.nix
@@ -19,13 +19,29 @@
         packages.default = pkgs.buildDotnetModule {
           pname = "mutannot";
           version = "0.1.0";
-          src = ./Mutannot;
-          projectFile = "Mutannot.fsproj";
-          nugetDeps = ./Mutannot/deps.json;
+          src = ./.;
+          projectFile = [
+            "Mutannot/Mutannot.fsproj"
+          ];
+          nugetDeps = ./deps.json;
           executables = [ "mutannot" ];
           dotnet-sdk = pkgs.dotnet-sdk_10;
           dotnet-runtime = pkgs.dotnet-sdk_10;
           useDotnetFromEnv = true;
+
+          nativeBuildInputs = [ pkgs.git ];
+
+          doCheck = true;
+          testProjectFile = [
+            "Mutannot.Tests/Mutannot.Tests.fsproj"
+            "Mutannot.IntegrationTests/Mutannot.IntegrationTests.fsproj"
+          ];
+
+          preCheck = ''
+            git init
+            git add .
+            git -c user.email="nix@build" -c user.name="Nix" commit -m "init"
+          '';
 
           meta = {
             mainProgram = "mutannot";
